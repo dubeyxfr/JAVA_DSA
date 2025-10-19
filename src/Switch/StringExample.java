a6eb75bba123a5dab385bd5fa3a438fb962cfc4d
dubeyxfr <prashant250971@gmail.com>
2025-10-19 22:35:34 +0530

    new

---
+++ src/Switch/StringExample.java
@@ -0,0 +1,11 @@
+package Switch;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        return "How are you.";
+    }
+}
