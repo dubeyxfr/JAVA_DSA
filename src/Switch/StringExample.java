df868aeb973ba92c3d3b40b986f26fc672f70bf4
dubeyxfr <prashant250971@gmail.com>
2025-10-19 22:53:33 +0530

    mr

--- src/Switch/StringExample.java
+++ src/Switch/StringExample.java
@@ -4,8 +4,16 @@ public class StringExample {
     public static void main(String[] args) {
         String message = greet();
         System.out.println(message);
+        String personalised = myGreet("good Morning");
+        System.out.println(personalised);
     }
+    static String myGreet(String greet){
+        String message = "hello " +greet;
+        return message;
+    }
+
     static String greet(){
         return "How are you.";
     }
+
 }
