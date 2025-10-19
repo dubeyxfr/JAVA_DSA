677e7c52e52ce445ca4fbc1a586daace2af6c73c
dubeyxfr <prashant250971@gmail.com>
2025-10-19 14:41:39 +0530

    new

--- src/Switch/Switch_1.java
+++ src/Switch/Switch_1.java
@@ -7,18 +7,11 @@ public class Switch_1 {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Your Fruit Name: ");
         String fruit = in.next();
-        switch (fruit){
-            case "mango":
-                System.out.println("KIng of the Fruits");
-                break;
-            case "Apple":
-                System.out.println("Red sweet Fruit");
-                break;
-            case "Orange":
-                System.out.println("Round Fruits");
-                break;
-            default:
-                System.out.println("Enter Valid Fruit Name");
+        switch (fruit) {
+            case "mango" -> System.out.println("KIng of the Fruits");
+            case "Apple" -> System.out.println("Red sweet Fruit");
+            case "Orange" -> System.out.println("Round Fruits");
+            default -> System.out.println("Enter Valid Fruit Name");
         }
       
     }
