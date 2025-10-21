540080f963a255a70db7b9618c026b80644191a0
dubeyxfr <prashant250971@gmail.com>
2025-10-21 23:30:44 +0530

    new

--- src/Switch/VarArgs.java
+++ src/Switch/VarArgs.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class VarArgs {
     public static void main(String[] args) {
-    fun(2,3,4,5,6,90,8);//
+    fun(2,3,4,5,6,90,8);//its is Know as Variable length arguments.
     }
     static void fun(int...v){
         System.out.println(Arrays.toString(v));//change the arr into toString!!
