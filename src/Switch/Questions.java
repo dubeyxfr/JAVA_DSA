120cf62638fcded14194535d2f55ba75eeeaec10
dubeyxfr <prashant250971@gmail.com>
2025-10-22 12:19:05 +0530

    new

---
+++ src/Switch/Questions.java
@@ -0,0 +1,27 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class Questions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = in.nextInt();
+        System.out.println(isPrime(num));
+
+
+    }
+    static boolean isPrime(int num){
+        if (num <= 1){
+            return false;
+        }
+        int c = 2;
+        while (c*c <=num ){
+            if (num % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c * c > num;
+    }
+}
