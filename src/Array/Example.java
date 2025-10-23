3a5c35ba27bc377a7f16d06e0423cd01938931e2
dubeyxfr <prashant250971@gmail.com>
2025-10-23 22:12:26 +0530

    new

---
+++ src/Array/Example.java
@@ -0,0 +1,12 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Example {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[3] = 12;
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/Switch/Armstrong.java
+++ src/Switch/Armstrong.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Armstrong {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Number: ");
-        int num = in.nextInt();
-        System.out.println(num + " is Armstrong value that is " +ArmStrong(num));
-
+//        System.out.print("Number: ");
+//        int num = in.nextInt();
+//        System.out.println(num + " is Armstrong value that is " +ArmStrong(num));
+        for (int i = 100; i<1000; i++){
+            if (ArmStrong(i)) {
+                System.out.println(i + " ");
+            }
+        }
     }
     static boolean ArmStrong(int num){
         int Original = num;
