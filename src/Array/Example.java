54cd39f3b0ebd96936be3ac1f24c13ae602d6dc1
dubeyxfr <prashant250971@gmail.com>
2025-10-23 22:36:04 +0530

    new

--- src/Array/Example.java
+++ src/Array/Example.java
@@ -7,6 +7,9 @@ public class Example {
         int[] arr = new int[5];
         arr[0] = 1;
         arr[3] = 12;
+        System.out.println(arr[0]);
         System.out.println(Arrays.toString(arr));
+        String[] StrArr= new String[5];
+        System.out.println(StrArr[0]);
     }
 }
---
+++ src/Array/input.java
@@ -0,0 +1,17 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length;i++){
+            arr[i] = in.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
