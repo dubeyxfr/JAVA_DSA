d3314ab829e24496b4357adcc2d74281bd06e1cd
dubeyxfr <prashant250971@gmail.com>
2025-10-25 22:42:18 +0530

    new

---
+++ src/Array/ArrayListExample.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(1000000);
+        list.add(1000000);
+        list.add(1000000);
+        list.add(1000000);
+        list.add(1000000);
+        System.out.println(list);
+
+    }
+}
