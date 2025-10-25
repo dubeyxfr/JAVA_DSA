73c6e0151a5a38e5fd730bc7d368e30403f6bfae
dubeyxfr <prashant250971@gmail.com>
2025-10-25 22:54:34 +0530

    new

--- src/Array/ArrayListExample.java
+++ src/Array/ArrayListExample.java
@@ -1,16 +1,30 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>(10);
-        list.add(1000000);
-        list.add(1000000);
-        list.add(1000000);
-        list.add(1000000);
-        list.add(1000000);
-        System.out.println(list);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);
+//        list.add(1000000);list.add(1000000);
+//        System.out.println(list);
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i));
+        }
 
     }
 }
