6399ffcda463606dd53d12a615d5e462d42e4279
dubeyxfr <prashant250971@gmail.com>
2025-10-27 21:53:50 +0530

    new

--- src/Array/SwapArr.java
+++ src/Array/SwapArr.java
@@ -7,7 +7,8 @@ public class SwapArr {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = {1,2,3,4};
-        swap(arr,2,3);
+//        swap(arr,2,3);
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void swap(int[] arr,int index1,int index2){
@@ -15,5 +16,14 @@ public class SwapArr {
         arr[index1] = arr[index2];
         arr[index2] = temp;
 
+    }
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
     }
 }
