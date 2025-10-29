9a5b01dfffa58155c4a58a1c8ee9e34f3fd0dd38
dubeyxfr <prashant250971@gmail.com>
2025-10-29 14:05:13 +0530

    new

--- src/Array/SearchInString.java
+++ src/Array/SearchInString.java
@@ -11,8 +11,14 @@ public class SearchInString {
         if (str.length() == 0){
             return false;
         }
-        for (int i = 0; i < str.length(); i++) {
-            if (target == str.charAt(i)){
+//        for (int i = 0; i < str.length(); i++) {
+//            if (target == str.charAt(i)){
+//                return true;
+//            }
+//        }
+        //By using For each loop!!!
+        for (char ch :  str.toCharArray()){
+            if (ch == target){
                 return true;
             }
         }
