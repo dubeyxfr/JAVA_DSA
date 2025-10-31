f77ec98a43795ce906cf3e2b4f576b94feadca83
dubeyxfr <prashant250971@gmail.com>
2025-10-31 12:59:23 +0530

    new

--- src/Array/BinarySearch.java
+++ src/Array/BinarySearch.java
@@ -2,28 +2,28 @@ package Array;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         int target = 7;
-        int ans = binarySearch(arr,target);
-        System.out.println("Your ans is " +ans);
+        int ans = binarySearch(arr, target);
+        System.out.println("At the index no " + ans);
     }
-    static int binarySearch(int[] arr, int target){
+
+    static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-        while(start <= end){
-            int mid = start + (end - start);
-            if (target < mid){
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
                 end = mid - 1;
-            } else if (target > mid) {
-                end = mid + 1;
-
-            }else {
-                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid; // target found
             }
-
-
         }
-        return -1;
 
+        return -1; // target not found
     }
 }
