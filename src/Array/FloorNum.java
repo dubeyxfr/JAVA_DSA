217c9570e1c639485401073f4a17092748741472
dubeyxfr <prashant250971@gmail.com>
2025-11-01 11:27:44 +0530

    new

--- src/Array/FloorNum.java
+++ src/Array/FloorNum.java
@@ -1,4 +1,29 @@
 package Array;
 
-public class Floor {
+public class FloorNum {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 9, 14, 16, 18};
+        int target = 15;
+        System.out.println(floorNum(arr, target));
+    }
+
+    static int floorNum(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        if (target < arr[0]) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return arr[end];
+    }
 }
--- src/Array/ceilingNum.java
+++ src/Array/ceilingNum.java
@@ -5,13 +5,17 @@ import javax.xml.stream.events.StartDocument;
 public class ceilingNum {
     public static void main(String[] args) {
         int[] arr = {2,3,5,9,14,16,18};
-        int target = 15;
+        int target = 19;
         System.out.println(ceiling(arr,target));
     }
     static int ceiling(int[] arr,int target){
+        if (target > arr.length-1){
+            return -1;
+        }
         int start = 0;
         int end = arr.length - 1;
         while(start <= end){
+
             int mid = start + (end - start)/2;
             if (target < arr[mid]){
                 end = mid - 1;
