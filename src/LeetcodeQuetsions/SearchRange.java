fe58d6fc20f9be7fca55a029294a1e495921e15f
dubeyxfr <prashant250971@gmail.com>
2025-11-03 13:29:09 +0530

    new

---
+++ src/LeetcodeQuetsions/SearchRange.java
@@ -0,0 +1,44 @@
+package LeetcodeQuetsions;
+
+public class SearchRange {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        System.out.println();
+
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int start = search(nums,target, true);
+        int end = search(nums,target,false);
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+    int search(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        while (start <= end) {
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            //potensial ans is found!!
+            else {
+                ans = mid;
+                if (findStartIndex == true){
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
